dc1c31d1592a6a1fb6a7398d78eca15e03015142
MumenMansour <mumenmansour97@gmail.com>
2022-11-23 19:35:21 -0300

    Changes to be committed: 	new file:   assets/bg.svg 	new file:   assets/icon-brazil.svg 	new file:   assets/icon-serbia.svg 	new file:   index.html 	new file:   main.js 	new file:   style.css

---
+++ main.js
@@ -0,0 +1,47 @@
+function criaJogos(time1, hora, time2) {
+    return `
+<li>
+    <img src="./assets/icon-${time1}.svg" alt="">
+        <strong>${hora}</strong>
+    <img src="./assets/icon-${time2}.svg" alt="">
+</li>
+    `
+}
+
+function criarCard(data, dia, jogos) {
+    return `
+    <div class="card">
+    <h2>${data} <span>${dia}</span></h2>
+    <ul>
+        ${jogos}
+    </ul>
+    </div>
+    `
+}
+
+document.querySelector('body').innerHTML =
+    `
+<header>
+        Calendário da Copa
+</header>
+    <main id="cards">      
+        ${criarCard(
+            "24/11",
+            "quinta",
+            criaJogos("brazil", "16:00", "serbia") +
+            criaJogos("brazil", "20:00", "serbia")
+        )}
+        ${criarCard(
+            "24/11",
+            "quinta",
+            criaJogos("brazil", "16:00", "serbia")
+        )}
+        ${criarCard(
+            "24/11",
+            "quinta",
+            criaJogos("brazil", "16:00", "serbia")
+        )}
+
+    </main>
+    
+`
